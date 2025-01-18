e132b3579f933e20a3577497dbf9a1889718efb4
Simone Motta <simone.motta.wd@gmail.com>
2025-01-18 17:18:19 +0100

    Added header, hero and skill section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 console.log("Typescript collegato");
+
+document.querySelectorAll<HTMLButtonElement>('.skillDescription-btn').forEach(button => {
+    button.addEventListener('click', (event: MouseEvent) => {
+        // Trova l'elemento che questo pulsante controlla
+        const targetSelector = button.getAttribute('data-bs-target');
+        const isExpanded = button.getAttribute('aria-expanded');
+        if (!targetSelector) return;
+        console.log(isExpanded);
+
+        const collapseElement = document.querySelector(targetSelector) as HTMLElement;
+        
+        // // Verifica se l'elemento è già aperto
+        if (isExpanded) {
+            collapseElement.classList.add('show');
+            button.classList.remove('collapsed');
+        } else {
+            collapseElement.classList.remove('show')
+        }
+    });
+});
